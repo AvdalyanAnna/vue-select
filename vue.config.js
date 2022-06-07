3ef159c3b79eaa39a7306186948cd2456c38eab6
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2022-06-07 12:57:31 +0400

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/',
+  publicPath: '/vue-select/',
   assetsDir: '',
   productionSourceMap: false,
   filenameHashing: true
